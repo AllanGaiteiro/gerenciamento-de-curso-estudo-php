b777f7e7804d5e71f438f974a5bbadb67e777e1d
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2021-06-14 20:12:05 -0300

    tela-login lgout

---
+++ public/login.php
@@ -0,0 +1,16 @@
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+<form>
+    <div class="form-group">
+        <label for="login">login</label>
+        <input type="text" class="form-control" id="login" name="login" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+</form>
---
+++ public/logout.php
@@ -0,0 +1,43 @@
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+<form>
+    <div class="form-group">
+        <label for="login">login</label>
+        <input type="text" class="form-control" id="login" name="login" placeholder="login" required>
+    </div>
+    <div class="form-group">
+        <label for="password">Senha</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Senha" required>
+    </div>
+    <div class="form-group">
+        <label for="confirm-password">Confirmar Senha</label>
+        <input type="password" class="form-control" id="confirm-password" name="confirm-password" placeholder="Confirmar Senha" required>
+    </div>
+    <div class="form-group">
+        <label for="name">Nome</label>
+        <input type="text" class="form-control" id="name" name="name"  placeholder="Nome" required>
+    </div>
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" name="email"  placeholder="Email" required>
+    </div>
+    <div class="form-group">
+        <label for="cpf">CPF</label>
+        <input type="text" class="form-control" id="cpf" name="cpf"  placeholder="CPF" required>
+    </div>
+    <div class="form-group">
+        <label for="photo">Foto</label>
+        <input type="text" class="form-control" id="photo" name="photo"  placeholder="Foto" required>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Cadastrar</button>
+</form>
+
+<div class="row">
+    <form class="col s12">
+
+    </form>
+</div>
