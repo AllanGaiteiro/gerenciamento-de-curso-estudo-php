be4af840fe4065c33dc525cfa79f3a30c7df5168
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2021-06-17 22:58:48 -0300

    estrutura base de pastas e arquivos

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+include('./server/routes.php');
+include('public/layouts/template.php');
+?>
---
+++ public/pages/login.php
@@ -0,0 +1,12 @@
+
+<form>
+    <div class="form-group">
+        <label for="login">login</label>
+        <input type="text" class="form-control" id="login" name="login" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+</form>
