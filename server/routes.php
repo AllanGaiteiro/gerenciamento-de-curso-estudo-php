70429eda1d3994afb3a5f086ef52c34b04e51fa1
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2021-06-19 20:13:50 -0300

    tela login

--- server/routes.php
+++ server/routes.php
@@ -6,48 +6,50 @@ function routes(/*string $page */)
     $url = $_SERVER['REQUEST_URI'];
     $urlInit = "/gerenciamento-de-curso-php";
     $method = $_SERVER['REQUEST_METHOD'];
-    if ($method === 'GET') {
+    ///$method === 'GET'
+    // routes
+    switch ($url) {
+        case $urlInit . "/":
+            include('public/pages/home.php');
+            break;
 
-        // routes
-        switch ($url) {
-            case $urlInit . "/":
-                include('public/pages/home.php');
-                break;
+        case $urlInit . "/home":
+            include('public/pages/home.php');
+            break;
 
-            case $urlInit . "/home":
-                include('public/pages/home.php');
-                break;
-
-            case $urlInit .  "/entrar":
-                include('public/pages/login.php');
-                break;
-
-            case $urlInit .  "/cadastrar":
-                include('public/pages/cadastro.php');
-                break;
-
-            case $urlInit .  "/listagem":
-                include('public/pages/listagem.php');
-                break;
-
-            case $urlInit .  "/adicionar-curso":
-                include('public/pages/adcionar-editar.php');
-                break;
-
-            case $urlInit .  "/editar-curso":
-                include('public/pages/adcionar-editar.php');
-                break;
-
-            case $urlInit .  "/sobre":
-                include('public/pages/sobre.php');
-                break;
-
-            case $urlInit .  "/contato":
-                include('public/pages/cotato.php');
-                break;
-            default:
+        case $urlInit .  "/entrar":
+            if ($method === 'POST') {
+                include('login-service.php');
+            }
             include('public/pages/login.php');
-                break;
-        }
+            break;
+
+
+        case $urlInit .  "/cadastrar":
+            include('public/pages/cadastro.php');
+            break;
+
+        case $urlInit .  "/listagem":
+            include('public/pages/listagem.php');
+            break;
+
+        case $urlInit .  "/adicionar-curso":
+            include('public/pages/adcionar-editar.php');
+            break;
+
+        case $urlInit .  "/editar-curso":
+            include('public/pages/adcionar-editar.php');
+            break;
+
+        case $urlInit .  "/sobre":
+            include('public/pages/sobre.php');
+            break;
+
+        case $urlInit .  "/contato":
+            include('public/pages/cotato.php');
+            break;
+        default:
+        include('public/pages/login.php');
+            break;
     }
 }
